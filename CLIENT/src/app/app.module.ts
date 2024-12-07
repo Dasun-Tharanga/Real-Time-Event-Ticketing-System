c924d0329de572ec03f1ce55ad8f21be46e0fefc
Dasun-Tharanga <dasun.20233188@iit.ac.lk>
2024-12-07 18:34:48 +0530

    Added Angular Material

---
+++ CLIENT/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MaterialModule } from './material/material.module';
+import { CustomerComponent } from './customer/customer.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule
+  ],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ CLIENT/src/app/material/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+
+const MaterialComponent = [
+  MatButtonModule,
+  MatIconModule,
+  MatCardModule
+];
+
+@NgModule({
+  imports: [MaterialComponent],
+  exports: [MaterialComponent]
+})
+export class MaterialModule { }
